a8d0d3bf7fca979d3d7096729dd8392d6f1277c7
adayag <a.dayag@gmail.com>
2013-10-16 17:56:25 -0500

    create startup cmodel that uses active model instead of active record

---
+++ app/models/startup.rb
@@ -0,0 +1,25 @@
+class Startup
+	include ActiveModel::Conversion
+	extend ActiveModel::Naming
+	attr_accessor :name, :logo_url, :city, :state, :product_description
+	attr_accessor :market_names, :high_concept_pitch, :angel_list_url
+
+	def initialize(attributes={})
+		@market_names = []
+		attributes.each do |key, value|
+			send "#{key}=", value
+		end
+	end
+
+	def persisted?
+		false
+	end
+
+	def add_market(market)
+		@market_names << market
+	end
+
+	def market_names
+		@market_names.join("\n")
+	end
+end
