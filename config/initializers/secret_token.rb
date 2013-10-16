9e0217fe83e56f3f49254d36a81931c185c9273c
adayag <a.dayag@gmail.com>
2013-10-16 16:42:46 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngelListApi::Application.config.secret_token = '3110c1fff892fdb47adb549eda77a80f0d42d4a2f77e557f5e9ad56c4164e98a8860e4f7ca0dad6f6d64ce6b9cd351e164206c8023f787fa06506e4134c5c894'
