ba2fddb96349efe9df953fea2ff5d04ac111a01d
adayag <a.dayag@gmail.com>
2013-10-17 00:37:16 -0500

    create module to handle url and make angel list api call

---
+++ lib/angel_list_parser.rb
@@ -0,0 +1,26 @@
+require 'open-uri'
+require 'json'
+
+module AngelListParser
+
+	def open_url(url)
+		url = strip_url(url)
+		angellist = open("http://api.angel.co/1/startups/search.json?domain=#{url}") #will open-uri escape
+		to_json(angellist.read)
+	end
+
+ private
+
+	def to_json(body)
+		JSON.parse(body)
+	end
+
+	def strip_url(url)
+	  url.sub!(/https\:\/\/www./, '') if url.include? "https://www."
+	  url.sub!(/http\:\/\/www./, '')  if url.include? "http://www."
+	  url.sub!(/www./, '')            if url.include? "www."
+	  return url
+  end
+
+  extend self
+end
